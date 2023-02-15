261663c1ac7d2c0c6f0b136769a580260e5f03a1
Dirk Vanden Boer <dirk.vdb@gmail.com>
2023-02-15 08:18:59 +0100

    Work on dataprocessor

--- src/dataprocessor.rs
+++ src/dataprocessor.rs
@@ -1,11 +1,12 @@
-use crc16::{MODBUS, State};
+use std::iter::zip;
+
+use crc16::{State, MODBUS};
 
 use crate::ProxyError;
 
-const HEADER_SIZE:usize = 8;
+const HEADER_SIZE: usize = 8;
 
-pub enum FieldType
-{
+pub enum FieldType {
     Text,
     Numeric,
 }
@@ -13,27 +14,27 @@ pub enum FieldType
 pub struct FieldSpecification {
     name: String,
     field_type: FieldType,
-    offset: u16,
-    length: u8,
+    offset: usize,
+    length: usize,
     divide: u16,
 }
 
 impl FieldSpecification {
-    pub fn text(name: &str, offset: u16, length: u8) -> FieldSpecification {
+    pub fn text(name: &str, offset: usize, length: usize) -> FieldSpecification {
         FieldSpecification {
             name: name.to_string(),
             field_type: FieldType::Text,
-            offset,
+            offset: offset / 2,
             length,
             divide: 0,
         }
     }
 
-    pub fn number(name: &str, offset: u16, length: u8, divide: u16) -> FieldSpecification {
+    pub fn number(name: &str, offset: usize, length: usize, divide: u16) -> FieldSpecification {
         FieldSpecification {
             name: name.to_string(),
             field_type: FieldType::Numeric,
-            offset,
+            offset: offset / 2,
             length,
             divide,
         }
@@ -41,6 +42,7 @@ impl FieldSpecification {
 }
 
 pub struct LayoutSpecification {
+    decrypt: bool,
     fields: Vec<FieldSpecification>,
 }
 
@@ -79,38 +81,47 @@ pub struct GrowattData {
 }
 
 impl GrowattData {
+    fn decrypt(growatt_data: &mut [u8]) {
+        static MASK: &[u8; 7] = b"Growatt";
+
+        // decrypt the data
+        for (data_val, mask_val) in zip(growatt_data[8..].iter_mut(), MASK.iter().cycle()) {
+            *data_val ^= *mask_val;
+        }
+    }
+
     fn validate_integity(data: &[u8]) -> Result<(), ProxyError> {
         let size = data.len();
-        if size < 10 {
-            return Err(ProxyError::RuntimeError(String::from("Invalid data packet")));
-        }
-
         let header_payload_length = u16::from_be_bytes(data[4..6].try_into()?) as usize;
         let actual_payload_length = size - HEADER_SIZE;
 
         if header_payload_length != actual_payload_length {
-            return Err(ProxyError::RuntimeError(format!("Data payload size error: expected {header_payload_length} bytes got {actual_payload_length}")));
+            return Err(ProxyError::RuntimeError(format!(
+                "Data payload size error: expected {header_payload_length} bytes got {actual_payload_length}"
+            )));
         }
 
         let protocol = data[3];
         if protocol == 0x05 || protocol == 0x06 {
-            let header_crc = u16::from_be_bytes(data[size-2..].try_into()?);
+            let header_crc = u16::from_be_bytes(data[size - 2..].try_into()?);
             let actual_crc = State::<MODBUS>::calculate(&data[..size - 2]);
 
             if header_crc != actual_crc {
-                return Err(ProxyError::RuntimeError(format!("Crc mismatch: expected {header_crc} got {actual_crc}")));
+                return Err(ProxyError::RuntimeError(format!(
+                    "Crc mismatch: expected {header_crc} got {actual_crc}"
+                )));
             }
 
             log::debug!("CRC Matched!");
         }
 
-        Ok (())
+        Ok(())
     }
 
-    fn check_header(header: &[u8;HEADER_SIZE]) -> Result<(), ProxyError> {
+    fn check_header(header: &[u8; HEADER_SIZE]) -> Result<(), ProxyError> {
         log::debug!("Header: {:02x?}", header);
 
-        let is_smart_meter=  header[7] == 0x20 || header[7] == 0x1b;
+        let is_smart_meter = header[7] == 0x20 || header[7] == 0x1b;
         log::debug!("Smart meter: {is_smart_meter}");
 
         log::debug!("Layout: T{:02x}{:02x}{:02x}", header[3], header[6], header[7]);
@@ -118,12 +129,57 @@ impl GrowattData {
         Ok(())
     }
 
-    pub fn from_buffer(growatt_data: &[u8], spec: &LayoutSpecification) -> Result<GrowattData, ProxyError> {
+    pub fn from_buffer(growatt_data: &mut [u8], spec: &LayoutSpecification) -> Result<GrowattData, ProxyError> {
+        if growatt_data.len() < 12 {
+            // ACK message
+            return Err(ProxyError::ParseError);
+        }
+
         GrowattData::validate_integity(growatt_data)?;
         GrowattData::check_header(&growatt_data[0..8].try_into()?)?;
 
+        if spec.decrypt {
+            GrowattData::decrypt(growatt_data);
+            let mut file = std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open("/Users/dirk/growatt.bin")?;
+
+            std::io::Write::write_all(&mut file, &growatt_data);
+        }
+
+        for field in &spec.fields {
+            let data_slice = &growatt_data[field.offset..field.offset + field.length];
+            match field.field_type {
+                FieldType::Text => {
+                    log::info!(
+                        "Text field: {} = {}",
+                        field.name,
+                        std::str::from_utf8(&data_slice)
+                            .expect(format!("Invalid string: {:02x?}", data_slice).as_str())
+                    );
+                }
+
+                FieldType::Numeric => {
+                    if field.length == 2 {
+                        log::info!(
+                            "Numeric field: {} = {}",
+                            field.name,
+                            u16::from_be_bytes(data_slice.try_into().expect("Invalid u16 length"))
+                        );
+                    } else if field.length == 4 {
+                        log::info!(
+                            "Numeric field: {} = {}",
+                            field.name,
+                            u32::from_be_bytes(data_slice.try_into().expect("Invalid u32 length"))
+                        );
+                    }
+                }
+            }
+        }
+
         Ok(GrowattData {
-            datalogserial: String::from(""),  
+            datalogserial: String::from(""),
             pvserial: String::from(""),
         })
     }
@@ -131,14 +187,13 @@ impl GrowattData {
 
 #[cfg(test)]
 mod tests {
-    use crate::dataprocessor::LayoutSpecification;
     use crate::dataprocessor::FieldSpecification;
+    use crate::dataprocessor::LayoutSpecification;
 
     use super::GrowattData;
 
     fn init() {
-        let _ = env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug"))
-        .init();
+        let _ = env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
     }
 
     #[test]
@@ -146,47 +201,48 @@ mod tests {
         init();
 
         let t065004x = LayoutSpecification {
+            decrypt: true,
             fields: Vec::from([
                 FieldSpecification::text("pvserial", 76, 10),
                 FieldSpecification::number("date", 136, 1, 10),
-                FieldSpecification::number("pvstatus", 158, 2, 0 ),
-                FieldSpecification::number("pvpowerin", 162, 4 ,  10),      
-                FieldSpecification::number("pv1voltage",  170, 2 , 10),      
-                FieldSpecification::number("pv1current",  174, 2 , 10),                      
-                FieldSpecification::number("pv1watt", 178, 4 , 10),                    
-                FieldSpecification::number("pv2voltage",  186, 2 , 10),              
-                FieldSpecification::number("pv2current",  190, 2 , 10),              
-                FieldSpecification::number("pv2watt", 194, 4 , 10),              
-                FieldSpecification::number("pvpowerout",  250, 4 , 10),              
-                FieldSpecification::number("pvfrequentie", 258, 2 , 100),              
-                FieldSpecification::number("pvgridvoltage", 262, 2 , 10),              
-                FieldSpecification::number("pvgridcurrent", 266, 2 , 10),              
-                FieldSpecification::number("pvgridpower",  270, 4 , 10),              
-                FieldSpecification::number("pvgridvoltage2", 278, 2 , 10),              
-                FieldSpecification::number("pvgridcurrent2", 282, 2 , 10),              
-                FieldSpecification::number("pvgridpower2", 286, 4 , 10),              
-                FieldSpecification::number("pvgridvoltage3", 294, 2 , 10),              
-                FieldSpecification::number("pvgridcurrent3", 298, 2 , 10),              
-                FieldSpecification::number("pvgridpower3", 302, 4 , 10),
-                FieldSpecification::number("totworktime",  346, 4 , 7200),
-                FieldSpecification::number("pvenergytoday", 354, 4 , 10),                
-                FieldSpecification::number("pvenergytotal", 362, 4 , 10),
-                FieldSpecification::number("epvtotal", 370, 4 , 10),
-                FieldSpecification::number("epv1today", 378, 4 , 10),              
-                FieldSpecification::number("epv1total", 386, 4 , 10),              
-                FieldSpecification::number("epv2today", 394, 4 , 10),              
-                FieldSpecification::number("epv2total", 402, 4 , 10),                         
-                FieldSpecification::number("pvtemperature", 530, 2 , 10),               
-                FieldSpecification::number("pvipmtemperature", 534, 2 , 10),        
-                FieldSpecification::number("pbusvolt", 550, 2 , 1),                
-                FieldSpecification::number("nbusvolt", 554, 2 , 1),                          
+                FieldSpecification::number("pvstatus", 158, 2, 0),
+                FieldSpecification::number("pvpowerin", 162, 4, 10),
+                FieldSpecification::number("pv1voltage", 170, 2, 10),
+                FieldSpecification::number("pv1current", 174, 2, 10),
+                FieldSpecification::number("pv1watt", 178, 4, 10),
+                FieldSpecification::number("pv2voltage", 186, 2, 10),
+                FieldSpecification::number("pv2current", 190, 2, 10),
+                FieldSpecification::number("pv2watt", 194, 4, 10),
+                FieldSpecification::number("pvpowerout", 250, 4, 10),
+                FieldSpecification::number("pvfrequentie", 258, 2, 100),
+                FieldSpecification::number("pvgridvoltage", 262, 2, 10),
+                FieldSpecification::number("pvgridcurrent", 266, 2, 10),
+                FieldSpecification::number("pvgridpower", 270, 4, 10),
+                FieldSpecification::number("pvgridvoltage2", 278, 2, 10),
+                FieldSpecification::number("pvgridcurrent2", 282, 2, 10),
+                FieldSpecification::number("pvgridpower2", 286, 4, 10),
+                FieldSpecification::number("pvgridvoltage3", 294, 2, 10),
+                FieldSpecification::number("pvgridcurrent3", 298, 2, 10),
+                FieldSpecification::number("pvgridpower3", 302, 4, 10),
+                FieldSpecification::number("totworktime", 346, 4, 7200),
+                FieldSpecification::number("pvenergytoday", 354, 4, 10),
+                FieldSpecification::number("pvenergytotal", 362, 4, 10),
+                FieldSpecification::number("epvtotal", 370, 4, 10),
+                FieldSpecification::number("epv1today", 378, 4, 10),
+                FieldSpecification::number("epv1total", 386, 4, 10),
+                FieldSpecification::number("epv2today", 394, 4, 10),
+                FieldSpecification::number("epv2total", 402, 4, 10),
+                FieldSpecification::number("pvtemperature", 530, 2, 10),
+                FieldSpecification::number("pvipmtemperature", 534, 2, 10),
+                FieldSpecification::number("pbusvolt", 550, 2, 1),
+                FieldSpecification::number("nbusvolt", 554, 2, 1),
             ]),
         };
 
         let growatt_data = include_bytes!("./testdata/growatt_1.bin");
+        let mut data = growatt_data.to_vec();
 
-        let gd = GrowattData::from_buffer(growatt_data, &t065004x).unwrap();
+        let gd = GrowattData::from_buffer(&mut data, &t065004x).unwrap();
         assert_eq!(gd.datalogserial, "serial");
-
     }
 }
